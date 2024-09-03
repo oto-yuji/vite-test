8fce20ad45de37049fcdb45778088cf2af03719b
oto-yuji <yuji.oto@divx.co.jp>
2024-09-03 18:43:48 +0900

    axiosInstanceのbaseURLを本番用に

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8001/',
+  baseURL: 'https://vite-test-one-xi.vercel.app',
 });
 
 export default axiosInstance;
+
